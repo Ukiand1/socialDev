d864630cf07fe5d5a8f53c2d8c885fe54f4c20ba
Ukiand1 <Uros@MacBook-Pro.local>
2016-09-20 11:56:01 +0200

    Added pods and changed the name of ViewController

---
+++ SocialDev/SignVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialDev
+//
+//  Created by Uroš Đurđević on 20/09/16.
+//  Copyright © 2016 Uroš Đurđević. All rights reserved.
+//
+
+import UIKit
+
+class SignVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
