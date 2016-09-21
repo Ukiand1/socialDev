9a098ea64b25227f88d1941a096b161c838acc4b
Ukiand1 <Uros@MacBook-Pro.local>
2016-09-21 16:11:38 +0200

    Added FeedView, and View's inside it

--- SocialDev/CircleView.swift
+++ SocialDev/CircleView.swift
@@ -1,24 +1,28 @@
 //
-//  PrittyView.swift
+//  CircleView.swift
 //  SocialDev
 //
-//  Created by Uroš Đurđević on 20/09/16.
+//  Created by Uroš Đurđević on 21/09/16.
 //  Copyright © 2016 Uroš Đurđević. All rights reserved.
 //
 
 import UIKit
 
-class PrittyView: UIView {
-
+class CircleView: UIImageView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
     }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
+    }
+    
     
     
 
